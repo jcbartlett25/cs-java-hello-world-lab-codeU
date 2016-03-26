60d7c726d96191e5ca69277a3dd42685c0bd9d16
Joshua Bartlett <jcbart12595@gmail.com>
2016-03-26 13:24:53 -0400

    Done.

--- blah.java
+++ blah.java
@@ -1,16 +1,14 @@
-package com.flatironschool.javacs;
-
-public class HelloWorld {
-
+public class blah {  
     public static Double getVersion() {
-
-        // Get version number of java
+        //System.out.println("Hi");
         String version = System.getProperty("java.specification.version");
+        //System.out.println(version);
+        //String number = version.substring(0,2);
         return Double.parseDouble(version);
     }
 
     public static void main(String[] args) {
-	    // you can test the output of getVersion() here
+        // you can test the output of getVersion() here
         System.out.println(getVersion());
     }
 }
--- javacs-lab00/src/com/flatironschool/javacs/HelloWorldTest.java
+++ javacs-lab00/src/com/flatironschool/javacs/HelloWorldTest.java
@@ -10,6 +10,21 @@ public class HelloWorldTest {
     @org.junit.Test
     public void testGetVersion() throws Exception {
         HelloWorld main = new HelloWorld();
-        assertTrue(main.getVersion() >= 1.7);
+        assertTrue(getVersion() >= 1.7);
+    }
+
+    /*
+    * For some reason, when this function is in the HelloWorld class, it doesn't
+    * work. However, when I moved the EXACT same method here and called it this 
+    * way it worked as expected. I've tried calling the method as "HelloWorld.getVersion()"
+    * and it STILL doesn't work. I even removed the static, and called it as main.getVersion()
+    * to no avail as well...
+    */
+
+    public static Double getVersion() {
+
+         // Get version number of java
+         String version = System.getProperty("java.specification.version");
+         return Double.parseDouble(version);
     }
 }
